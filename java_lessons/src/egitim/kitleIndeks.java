9bd914d7e523c0e1fa499f4829e12c2660276eda
fatihkzlky <fkklan001@gmail.com>
2025-01-15 03:43:16 +0300

    kitleIndeks.java dosyası yüklendi

---
+++ java_lessons/src/egitim/kitleIndeks.java
@@ -0,0 +1,24 @@
+package src.egitim;
+
+import java.util.Scanner;
+
+public class kitleIndeks {
+    public static void main(String[] args) {
+
+        double kilo , indeks ;
+        double metre ;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+        System.out.print("Kilonuzu giriniz: ");
+        kilo = scanner.nextDouble();
+
+        System.out.print("Boyunuzu metre cinsinde giriniz: ");
+        metre = scanner.nextDouble();
+
+        indeks =  kilo / ( metre  * metre ) ;
+        System.out.print("Vücut kitle indeksiniz: " + indeks);
+
+        }
+
+    }
+}
